a026f6e6f17f9b08c3b1c645af7f9c53916c4843
Thomas McLaughlan <tmclaughlan@pushtechnology.com>
2015-02-23 14:42:01 +0000

    Fix the player GUI.

--- src/client/game/player.js
+++ src/client/game/player.js
@@ -5,18 +5,21 @@ var Hand = require('./hand');
 
 // Player GUIs
 var playerPosition = [
-    { x : 100, y : 200 },
-    { x : 1700, y : 200 },
-    { x : 100, y : 1200 },
-    { x : 1700, y : 1200 }
+    { x : 200, y : 200 },
+    { x : 1848, y : 200 },
+    { x : 200, y : 1336 },
+    { x : 1848, y : 1336 }
 ];
 
 function Player(app, session, turn, isPlayer) { 
     var pos = playerPosition[turn];
 
-    var score = Text(0, 140, 'Score : 0', 18);
-    var name = Text(0, 120, 'Player ' + turn, 18);
-    var icon = Text(100, 120, 'Playing', 18);
+    var yOffset = turn < 2 ? 0 : 315;
+    var xOffset = turn % 2 ? 96 : 0;
+
+    var score = Text(-60 - xOffset, 170 - yOffset, 'Score : 0', 28);
+    var name = Text(-60 - xOffset, 145 - yOffset, 'Player ' + turn, 28);
+    var icon = Text(60 - xOffset, 145 - yOffset, 'Playing', 28);
 
     var gui = PlayerGUI(pos.x, pos.y, name, score, icon);
 
